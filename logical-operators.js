b37e172f4d59cefeb6a1bc8cf09df070c0a18d9b
mounika <mounikamusham12@gmail.com>
2021-12-14 18:16:23 +0530

    conditional-operators

---
+++ logical-operators.js
@@ -0,0 +1,29 @@
+
+if(50<60 && 50>30 && 30<40){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+
+if(50<40 && 50>30 && 30<40){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+
+if(5>20 || 20>30 || 45<55){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+if(5>20 || 20>30 || 45>55){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+
+
